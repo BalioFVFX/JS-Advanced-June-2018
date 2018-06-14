d693344e35521153a9313325531dbd7b0f95e440
Erik <vfxbaliof@gmail.com>
2018-06-14 14:27:09 +0300

    02. Fill Dropdown

---
+++ EXERCISE DOM MANIPULATIONS/02. Fill Dropdown/dropdown.js
@@ -0,0 +1,12 @@
+function addItem() {
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let menu = document.getElementById('menu');
+
+    let option = document.createElement("option");
+    option.text = newItemText.value;
+    option.value = newItemValue.value;
+    menu.appendChild(option);
+    newItemText.value = '';
+    newItemValue.value = '';
+}
