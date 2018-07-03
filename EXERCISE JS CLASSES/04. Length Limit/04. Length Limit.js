f2960e5df34f5ca051019048e083d5a9dda02187
Erik <vfxbaliof@gmail.com>
2018-07-03 15:14:22 +0300

    04. Length Limit

---
+++ EXERCISE JS CLASSES/04. Length Limit/04. Length Limit.js
@@ -0,0 +1,60 @@
+class Stringer {
+    constructor(str, length) {
+        this.innerString = str;
+        this.innerLength = length;
+        this.originalStr = str;
+    }
+
+    decrease(length) {
+        this.innerLength = this.innerString.length;
+        this.innerLength -= length;
+        if (this.innerLength <= 0) {
+            this.innerString = '...';
+            this.innerLength = 0;
+        }
+        else {
+            this.innerString = this.originalStr.substring(0, this.innerLength);
+        }
+    }
+
+    increase(length) {
+        this.innerLength = this.innerString.length;
+        this.innerLength += length;
+        if (this.innerLength <= 0) {
+            this.innerString = '...';
+            this.innerLength = 0;
+        }
+        else {
+            this.innerString = this.originalStr.substring(0, this.innerLength);
+        }
+
+
+    }
+
+    toString() {
+        if (this.innerString === this.originalStr) {
+            return this.innerString;
+        }
+        if (this.innerLength === 0) {
+            return this.innerString;
+        }
+        return this.innerString + '...';
+
+    }
+}
+
+
+let test = new Stringer("Viktor", 6);
+test.decrease(9);
+console.log(test.toString());
+// let test = new Stringer("Test", 5);
+// console.log(test.toString()); //Test
+//
+// test.decrease(3);
+// console.log(test.toString()); //Te...
+//
+// test.decrease(5);
+// console.log(test.toString()); //...
+//
+// test.increase(4);
+// console.log(test.toString()); //Test
